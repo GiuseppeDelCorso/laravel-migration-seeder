bb524243bbfe52bf48f59fb14f1a838ae6cbee55
giuseppedelcorso <Giuseppedecoboolean@gmail.com>
2024-01-23 17:11:35 +0100

    stampo a schermo i dati del database e aggiungo del css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,12 +4,31 @@
 
 <div class="container">
         <div class="row">
-            @foreach ($train as $movies)
-                <div class="col-2">
-                    <h3>{{$movies->title}}</h3>
-                    <p>Titolo Originale -> {{$movies->original_title}}</p>
-                    <p>Lingua -> {{$movies->nationality}}</p>
-                    <p>Data -> {{$movies->date}}</p>
+            @foreach ($train as $trains)
+                <div class="col-4">
+                    <h3 class="py-2 fs-1">{{$trains->agency}}</h3>
+                    <p>Il Treno Parte dalla stazione di 
+                        <span class="red">{{$trains->departure_station}}</span> 
+                    </p>
+                    <p>Il Treno Arriva dalla stazione di
+                         <span class="red">{{$trains->arrival_station}}</span> 
+                    </p>
+                    <p>Il Treno parte alle ore 
+                        <span class="red">{{$trains->departure_time}}</span>
+                    </p>
+                    <p>Treno numero 
+                        <span class="red">{{$trains->train_code}}</span> 
+                    </p>
+                    <p>Carrozza numero
+                         <span class="red"> {{$trains->carriage_number}}</span>
+                    </p>
+                    <p>Il Treno e in orario
+                         <span class="red"> {{$trains->in_time}}</span> 
+                    </p>
+                    <p>Treno Cancellato 
+                        <span class="red">{{$trains->train_cancelled}}</span> 
+                    </p>
+                  
                 </div>
             @endforeach
         </div>
